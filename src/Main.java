78fad392aede8b107898a87d9a758b76c0159175
Tsuki <hung5008@gmail.com>
2025-05-23 16:10:17 +0700

    Small steps for man, giant leap for mankind

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Hello JavaFX!");
-        Scene scene = new Scene(label, 300, 200);
+        stage.initStyle(StageStyle.DECORATED);
+        BorderPane root = new BorderPane();
+        Button poker = new Button("Vietnamese Northern Poker");
+        Button threeCard = new Button("Three Cards Game");
+
+        HBox hbox = new HBox(5, poker, threeCard);
+
+        root.setCenter(hbox);
+
+        Scene scene = new Scene(root, 300, 200);
+
         stage.setScene(scene);
-        stage.setTitle("It works!");
+        stage.setTitle("Card Kingdom");
         stage.show();
     }
 
--- src/gui/CardGameScreen.java
+++ src/gui/CardGameScreen.java
@@ -1,6 +1,4 @@
 package gui;
 
-
-
 public class CardGameScreen {
 }
