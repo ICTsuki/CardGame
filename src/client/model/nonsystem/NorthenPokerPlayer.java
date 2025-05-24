1d877e3ab2eb0e043c0598ad4259a77c18bab400
Tsuki <hung5008@gmail.com>
2025-05-25 01:50:13 +0700

    Add three card game rule

--- src/client/model/nonsystem/NorthenPokerPlayer.java
+++ src/client/model/nonsystem/NorthenPokerPlayer.java
@@ -135,12 +135,12 @@ public class NorthenPokerPlayer extends Player {
             scanner.nextLine();
 
             if (t == 0) {
-                File file = new File("src/model/gamerule/Northern Poker/Rule - VietNamese.txt");
+                File file = new File("src/client/model/gamerule/Northern Poker/Rule - VietNamese.txt");
                 System.out.println("Absolute path: " + file.getAbsolutePath());
                 openTextFile(file);
                 break;
             } else if (t == 1) {
-                File file = new File("src/model/gamerule/Northern Poker/Rule - English.txt");
+                File file = new File("src/client/model/gamerule/Northern Poker/Rule - English.txt");
                 openTextFile(file);
                 break;
             } else {
